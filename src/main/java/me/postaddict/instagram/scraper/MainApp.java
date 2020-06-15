5ed5f34b1ad7f45a34ae0cdc52daac1628989c9d
Mojtaba <mojtaba@applyboard.com>
2020-06-15 00:07:13 -0400

    upgrade lombok. store getMedias result into a variable.

--- src/main/java/me/postaddict/instagram/scraper/MainApp.java
+++ src/main/java/me/postaddict/instagram/scraper/MainApp.java
@@ -1,6 +1,8 @@
 package me.postaddict.instagram.scraper;
 
 import me.postaddict.instagram.scraper.interceptor.UserAgents;
+import me.postaddict.instagram.scraper.model.Media;
+import me.postaddict.instagram.scraper.model.PageObject;
 import me.postaddict.instagram.scraper.model.Story;
 
 import java.io.IOException;
@@ -14,7 +16,7 @@ public class MainApp {
 
 //            Account account = instagram.getAccountByUsername("shiraz1400.ir");
             long [] ids = {2004595101, 470174409, 2097652665};
-            instagram.getMedias("shirazfood",1);
+            PageObject<Media> media = instagram.getMedias("shirazfood",1);
             Story story = instagram.getStory(ids);
 //            System.out.println(account.getMedia().getCount());
             System.out.println("hi");
